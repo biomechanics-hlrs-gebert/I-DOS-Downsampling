283db0d14ead9dc65a656fd117a593128b0159a4
JoGebert <johannes.gebert@hlrs.de>
2021-12-30 15:42:44 +0100

    small improvements on versioning and subtree

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
-CHARACTER(LEN=scl), PARAMETER :: revision = 'v2.0.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = '8ad5970198fe38033555a836fb06695c18651d53'
+CHARACTER(LEN=*), PARAMETER :: longname = 'X to Meta Converter'
+CHARACTER(LEN=*), PARAMETER :: revision = 'v2.0.0'
+CHARACTER(LEN=*), PARAMETER :: hash = 'df50010'
