028285b01249bc501ea9d14937ab3cdd08883101
JoGebert <johannes.gebert@hlrs.de>
2021-12-30 18:18:47 +0100

    Reefactoring

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
-CHARACTER(LEN=scl), PARAMETER :: revision = 'v0.1.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = '8064a41484170c8289cd93f5aee1c9170f3ad62b'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'Computed Tomography Image Filter'
+CHARACTER(LEN=scl), PARAMETER :: revision = 'V3.2.1'
+CHARACTER(LEN=scl), PARAMETER :: hash = 'bec88352a37f8fe981b00dedda7229a3c4a939a0'
