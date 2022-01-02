a1b2b4793f7e23da99cbeb5ab4d5fa8d6e10553e
Johannes Gebert <johannes.gebert@hlrs.de>
2022-01-02 19:53:06 +0100

    Update revision_meta.f90

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Computed Tomography Image Filter'
-CHARACTER(LEN=scl), PARAMETER :: revision = 'V3.2.1'
+CHARACTER(LEN=scl), PARAMETER :: revision = 'V5.0.0'
 CHARACTER(LEN=scl), PARAMETER :: hash = '2dd7fa080e2c39e54a5ed0d719657ba782933a43'
