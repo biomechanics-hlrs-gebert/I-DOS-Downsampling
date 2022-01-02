cd66edc9cd2b72a79931f9aae25f57d41a5266b5
JoGebert <johannes.gebert@hlrs.de>
2022-01-03 00:16:26 +0100

    Bugfixes, enhancements, features.... General dev

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'CT-Binarization'
-CHARACTER(LEN=scl), PARAMETER :: revision = ''
-CHARACTER(LEN=scl), PARAMETER :: hash = 'ca55537960b7e9eaa5d47153ef12c03f09a1bdad'
+CHARACTER(LEN=scl), PARAMETER :: revision = 'V1.0.0'
+CHARACTER(LEN=scl), PARAMETER :: hash = '3edc3343bea81b50aaac80ed05bbb7b27794eb0f'
