1ff3c7642295a69d1d96ba3528c4e80675fc9653
JoGebert <johannes.gebert@hlrs.de>
2022-01-03 23:49:43 +0100

    Squashed 'central_src/' changes from cd66edc..373846e
    373846e Enhancements, default integer 8!!
    9291928 Major rework
    
    git-subtree-dir: central_src
    git-subtree-split: 373846e72f66e5d0f0c689edfe2d3ca9723534e1

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'CT-Binarization'
-CHARACTER(LEN=scl), PARAMETER :: revision = 'V1.0.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = '3edc3343bea81b50aaac80ed05bbb7b27794eb0f'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'Directly Discretizing Tensor Computation'
+CHARACTER(LEN=scl), PARAMETER :: revision = ''
+CHARACTER(LEN=scl), PARAMETER :: hash = '8970b21142e9af2fe6b510b26cc4c148ca657941'
