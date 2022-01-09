ecdbe647cc4070c6a66eb0d3a77979a8f4f818f5
JoGebert <johannes.gebert@hlrs.de>
2022-01-09 17:17:05 +0100

    Bugfix

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'CT-Binarization'
 CHARACTER(LEN=scl), PARAMETER :: revision = 'V1.0.0'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'a5a7f09ec3837277ca9748292eadb57fc601db02'
+CHARACTER(LEN=scl), PARAMETER :: hash = 'dc295231fc31255fdcf56e7ed72a667a18080055'
