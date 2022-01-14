98aaeca0662b5704ed363c3e4be132129a8c8fc2
JoGebert <johannes.gebert@hlrs.de>
2022-01-15 00:18:09 +0100

    Compiles, no unsused variables

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,3 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Spatial Image Registration'
 CHARACTER(LEN=scl), PARAMETER :: revision = ''
-CHARACTER(LEN=scl), PARAMETER :: hash = '6cc0064bdcb4d6436380b9b773f3c290dcfb75e2'
+CHARACTER(LEN=scl), PARAMETER :: hash = '2558ee36b5f5a83ba2423c89566e2a457d366e23'
