28d9eb2fdf158d645d5fad3e367eb561d662b208
JoGebert <johannes.gebert@hlrs.de>
2022-01-16 19:08:36 +0100

    Refactoring

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'd8a0f7d04edc4ea120b2da91002e72421258dee3'
+CHARACTER(LEN=scl), PARAMETER :: hash = '9bed3af3c0c947e2eb44c82da31c992f64da2e07'
