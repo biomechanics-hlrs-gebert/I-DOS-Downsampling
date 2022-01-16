3ab2c60a3140b123f4f59b3e6b93199a2c0c8c7c
JoGebert <johannes.gebert@hlrs.de>
2022-01-16 02:30:46 +0100

    Squashed 'central_src/' changes from 98aaeca..a921dc8
    a921dc8 Release version
    7615e9f Generally working version
    81bc1a9 Initial compiling version
    f7350e4 Initial commit
    
    git-subtree-dir: central_src
    git-subtree-split: a921dc8fb7fc79344de55880d6d1a11d0653fb0b

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,3 +1,2 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'Spatial Image Registration'
-CHARACTER(LEN=scl), PARAMETER :: revision = ''
-CHARACTER(LEN=scl), PARAMETER :: hash = '2558ee36b5f5a83ba2423c89566e2a457d366e23'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'Downscaling'
+CHARACTER(LEN=scl), PARAMETER :: hash = '597aa67b22c9611a705b73d7b5c077f6656c2556'
