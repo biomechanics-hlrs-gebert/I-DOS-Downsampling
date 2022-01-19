cf25b7f391887943fe57d74c04f1c0c5842441df
JoGebert <johannes.gebert@hlrs.de>
2022-01-20 00:51:45 +0100

    Bugfix @ allocations

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
-CHARACTER(LEN=scl), PARAMETER :: hash = '6581e737acd63350e720cfb6424bb7b9e2c3bd08'
+CHARACTER(LEN=scl), PARAMETER :: hash = '3d9adc2b62443c926cbb43579019e4ff7e552d83'
