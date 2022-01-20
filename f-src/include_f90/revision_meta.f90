07a43980de0565d9fd3630efb0dc7cff0545da7d
JoGebert <johannes.gebert@hlrs.de>
2022-01-20 22:16:58 +0100

    Squashed 'central_src/' changes from cc9099c..e7e4517
    e7e4517 Enhancements on lock/temporary files
    95efddd Bugfix
    5e94cd8 Bugfix & Refactoring
    
    git-subtree-dir: central_src
    git-subtree-split: e7e4517fe7fa838f4cb7a7cda672a86a6f9308d0

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
-CHARACTER(LEN=scl), PARAMETER :: longname = 'Computed Tomography Image Filter'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'd5cb513b544f60c63cdf415f5d4bdd7b195ee1aa'
+CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
+CHARACTER(LEN=scl), PARAMETER :: hash = '6fa6c61ffb9c73f2b0200b9e0fcef7a6ebaff3ec'
