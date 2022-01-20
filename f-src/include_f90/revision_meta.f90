95efddd25479b0059e1734911d729a41d9041686
JoGebert <johannes.gebert@hlrs.de>
2022-01-20 21:10:43 +0100

    Bugfix

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'X to Meta Converter'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'ff68cfe2c48e89df62ef6193b7637302ccb4aff5'
+CHARACTER(LEN=scl), PARAMETER :: hash = '6fa6c61ffb9c73f2b0200b9e0fcef7a6ebaff3ec'
