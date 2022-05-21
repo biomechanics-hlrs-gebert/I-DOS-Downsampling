874f42c9085f53a7f565f5caecfccc62615366cb
JoGebert <johannes.gebert@hlrs.de>
2022-05-20 22:03:44 -0400

    refamed variables

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Spatial Image Registration'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'd32f95689f409a6d30d823de860d6f48c89b69c0'
+CHARACTER(LEN=scl), PARAMETER :: hash = 'a05301c8b458bf0db72374f8dd20abffa7f6aadb'
