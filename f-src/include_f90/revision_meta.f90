8e92a681a9cde38c2719b5e954ba3159a6114da0
JoGebert <johannes.gebert@hlrs.de>
2022-06-04 01:38:24 -0400

    Initial commit

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Spatial Image Registration'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'ff9b4519eba75463643df5cd52de07c787879adb'
+CHARACTER(LEN=scl), PARAMETER :: hash = '667b79dcd4266792ea20032a91ead84189c93845'
