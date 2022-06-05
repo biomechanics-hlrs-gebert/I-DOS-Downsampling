990e4bced201822d4f2d543c2461075cfef8b7fb
JoGebert <johannes.gebert@hlrs.de>
2022-06-05 01:47:10 -0400

    Write list of results, drop postprocess binary.

--- f-src/include_f90/revision_meta.f90
+++ f-src/include_f90/revision_meta.f90
@@ -1,2 +1,2 @@
 CHARACTER(LEN=scl), PARAMETER :: longname = 'Spatial Image Registration'
-CHARACTER(LEN=scl), PARAMETER :: hash = 'f1650ca2368521f05df1b9ead990dc1186e1f22a'
+CHARACTER(LEN=scl), PARAMETER :: hash = '70da3527ca80a2f1fca2ab7565c265c9c3acf676'
